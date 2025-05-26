744260d1e52230f8891b3f5504275c57348fa160
ImYousef1l <1221305727@student.mmu.edu.my>
2025-05-27 02:04:39 +0800

    modify the shown projects, modify the more button, Styling

--- script.js
+++ script.js
@@ -86,6 +86,10 @@ function createRepoCard(repo, language) {
 async function fetchRepos() {
   const response = await fetch(`https://api.github.com/users/${githubUsername}/repos?sort=updated&per_page=100`);
   allRepos = await response.json();
+
+  // Exclude the repo named exactly "ImY1l"
+  allRepos = allRepos.filter(repo => repo.name !== githubUsername);
+
   displayRepos();
 }
 
